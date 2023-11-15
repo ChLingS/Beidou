8b64e0bb302930241e29be360a9e9d871ec60059
ChLingS <chlis@jxnu.edu.cn>
2023-11-15 10:15:48 +0800

    第一版

---
+++ beidou/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports={
+    PluginL:{
+        autoprefixer:{
+            "postcss-pxtorem":{
+                "rootValue":75,
+                "propList":["*"]
+            }
+        }
+    }
+}
