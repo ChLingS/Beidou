08b8d9b65d7592e3816321926f3d985ced1e0916
ChLingS <147455543+ChLingS@users.noreply.github.com>
2023-11-21 20:09:59 +0800

    Update router.js

--- beidou/src/router/router.js
+++ beidou/src/router/router.js
@@ -1,7 +1,12 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import home from '../views/home.vue'
 const router = createRouter({
     history:createWebHashHistory(),
     routes:[
-        
+        {
+            path:'/',
+            component:home,
+        },
     ]
     })
+    export default router;
